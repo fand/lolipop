d9e2b4dc5fcf7894ef72965360d4f22ea1ce1ea5
fand <fand@gmork.in>
2014-11-09 13:48:36 +0900

    playlist

--- src/scripts/Player.js
+++ src/scripts/Player.js
@@ -2,39 +2,32 @@
 
 var Vue = require('vue');
 
-var fs = require('fs');
-console.log('fs');
-console.log(fs.readFile);
+var LoliPlaylist = require('./Playlist');
 
-var checker = document.createElement('audio');
-var isLoadable = function (path) {
-  if (fs.existsSync(path)) {
-    return true;
-    //if (checker.canPlayType)
-  }
-  return false;
-};
-
-var loadFile = function (file) {
-  console.log('isLoadable(file.path)');
-  console.log(isLoadable(file.path));
-  console.log('hoge');
-  console.log(isLoadable('hfdsosad'));
-  return true;
-};
-
-var Player = new Vue({
-  el: '#player',
+var PlayerPlaylist = new Vue({
+  el: '#player-playlist',
   data: {
     over: false
   },
   methods: {
     onDrop: function (files) {
-      for (var i = 0; i < files.length; i++) {
-        loadFile(files[i]);
-      }
+      this.$.playlist.onDrop(files);
+    },
+    getSongs: function () {
+      return this.$.playlist.songs;
     }
   }
 });
+
+var Player = new Vue({
+  el: '#player',
+  methods: {
+    onClick: function () {
+      console.log('yo');
+      console.log(PlayerPlaylist.getSongs());
+    }
+  }
+});
+
 
 module.exports = Player;
