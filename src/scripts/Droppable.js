52656eec5448e9890305089246b196204f6c931c
fand <fand@gmork.in>
2014-11-09 19:06:16 +0900

    change player vm structure

--- src/scripts/Droppable.js
+++ src/scripts/Droppable.js
@@ -7,23 +7,40 @@ var cancelEvent = function (e) {
   e.stopPropagation();
 };
 
-var Droppable = Vue.directive('droppable', function (callback) {
-  var self = this;
-  this.el.addEventListener('dragover', function (e) {
-    cancelEvent(e);
-    self.vm.$data.over = true;
-  });
-  this.el.addEventListener('dragleave', function (e) {
-    cancelEvent(e);
-    self.vm.$data.over = false;
-  });
-  this.el.addEventListener('drop', function (e) {
-    cancelEvent(e);
-    self.vm.$data.over = false;
-    if (callback) {
-      callback(e.dataTransfer.files);
+var Droppable = Vue.directive('droppable', {
+  data: {
+    first: true,
+    vm: null,
+    callback: null,
+    over: 'over'
+  },
+  bind: function (value) {
+    this.data[this.arg] = value;
+    this.data.vm = this.vm;
+    if (this.data.first) {
+      this.data.first = false;
+      var self = this;
+      this.el.addEventListener('dragover', function (e) {
+        cancelEvent(e);
+        self.data.vm.$data[self.data.over] = true;
+      });
+      this.el.addEventListener('dragleave', function (e) {
+        cancelEvent(e);
+        self.data.vm.$data[self.data.over] = false;
+      });
+      this.el.addEventListener('drop', function (e) {
+        cancelEvent(e);
+        self.data.vm.$data[self.data.over] = false;
+        if (self.data.callback) {
+          self.data.callback(e.dataTransfer.files);
+        }
+      });
     }
-  });
+  },
+  update: function (value) {
+    this.data[this.arg] = value;
+  }
 });
+
 
 module.exports = Droppable;
--- src/scripts/Player.js
+++ src/scripts/Player.js
@@ -1,33 +1,52 @@
 'use strict';
 
 var Vue = require('vue');
-
+var Droppable = require('./Droppable');
 var LoliPlaylist = require('./Playlist');
 
-var PlayerPlaylist = new Vue({
-  el: '#player-playlist',
-  data: {
-    over: false
-  },
-  methods: {
-    onDrop: function (files) {
-      this.$.playlist.onDrop(files);
-    },
-    getSongs: function () {
-      return this.$.playlist.songs;
-    }
+var fs = require('fs');
+var mime = require('mime');
+
+var checker = document.createElement('audio');
+var isLoadable = function (path) {
+  if (fs.existsSync(path)) {
+    var type = mime.lookup(path);
+    return !!(checker.canPlayType(type).replace('no', ''));
   }
-});
+  return false;
+};
+
+var loadFile = function (file) {
+  console.log(isLoadable(file.path));
+  return true;
+};
 
 var Player = new Vue({
   el: '#player',
+  data: {
+    songs: [],
+    overControl: false,
+    overList: false
+  },
   methods: {
-    onClick: function () {
+    play: function () {
       console.log('yo');
-      console.log(PlayerPlaylist.getSongs());
+    },
+    onDropList: function (files) {
+      var self = this;
+      for (var i = 0; i < files.length; i++) {
+        if (isLoadable(files[i].path)) {
+          var song = {
+            name: files[i].name,
+            path: files[i].path,
+            rate: 1.0
+          };
+          self.songs.push(song);
+          console.log(self.songs);
+        }
+      }
     }
   }
 });
-
 
 module.exports = Player;
