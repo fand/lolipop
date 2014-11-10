fa97d88d0edd7ad370cf61f887ccccb13d976ee6
fand <fand@gmork.in>
2014-11-10 17:57:40 +0900

    use twemoji

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,6 +5,12 @@ var Vue = require('vue');
 var Droppable = require('./Droppable');
 var Player = require('./Player');
 
+// Set window title
+var logo = twemoji.parse('\uD83D\uDC96\uD83D\uDC95\uD83D\uDC97');
+logo = logo + '<div>L  O  L  I  P  O  P</div>' + logo;
+document.getElementById('logo').innerHTML = logo;
+
+
 var main = new Vue({
   el: '#app',
   data: {
