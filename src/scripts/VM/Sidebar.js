322380760c49dcce8272968cd236bad68da5eeb3
fand <fand@gmork.in>
2015-10-21 01:47:36 +0900

    Rewrite Sidebar.js in ES6

--- src/scripts/VM/Sidebar.js
+++ src/scripts/VM/Sidebar.js
@@ -1,77 +1,88 @@
-'use strict';
+import Vue from 'vue';
+import Editable from './Editable';
 
-var Vue = require('vue');
+import template from './templates/Sidebar.html';
 
-var Editable = require('./Editable');
+const Sidebar = Vue.extend({
 
-var Sidebar = Vue.extend({
-  template: require('./templates/Sidebar.html'),
+  template,
+
   props: {
     collection      : Object,
     currentPlaylist : Number,
   },
+
   components : {
     editable : Editable,
   },
-  data: function () {
+
+  data () {
     return {
-      selectedPlaylists: [],
-      isSelected: [],
-      lastClick: {
-        time: new Date().getTime(),
-        index: 0
+      selectedPlaylists : [],
+      isSelected        : [],
+      lastClick : {
+        time  : new Date().getTime(),
+        index : 0,
       },
-      isDragging: false,
-      isPlaceholder: [],
-      dragOpts: {
-        insertPos: null,
-        elementsAfterInsert: []
+      isDragging    : false,
+      isPlaceholder : [],
+      dragOpts : {
+        insertPos           : null,
+        elementsAfterInsert : [],
       },
-      isEditingName: false
+      isEditingName : false,
     };
   },
-  computed: {
-    playlists: function () {
+
+  computed : {
+    playlists () {
       return this.collection.playlists;
-    }
+    },
   },
-  created: function () {
-    var self = this;
-    this.$watch('selectedPlaylists', function () {
-      if (! self.collection) { return; }
-      self.isSelected = new Array(self.collection.length);
-      for (var i = 0; i < self.collection.size(); i++) {
-        self.isSelected.$set(i, (self.selectedPlaylists.indexOf(i + "") !== -1));
+
+  created () {
+    this.$watch('selectedPlaylists', () => {
+      if (! this.collection) { return; }
+      this.isSelected = new Array(this.collection.length);
+      for (let i = 0; i < this.collection.size(); i++) {
+        this.isSelected.$set(i, (this.selectedPlaylists.indexOf(i + "") !== -1));
       }
     });
-    this.$watch('collection', function () {
+    this.$watch('collection', () => {
       this.selectedPlaylists = [];
     });
 
-    this.$on('editedPlaylistName', function (index, value) {
+    this.$on('editedPlaylistName', (index, value) => {
       this.collection.at(index).name = value;
     });
   },
-  methods: {
-    addPlaylist: function () {
+
+  methods : {
+
+    addPlaylist () {
       this.$dispatch('addPlaylist');
     },
-    playPlaylist: function (index) {
+
+    playPlaylist (index) {
       this.$dispatch('playPlaylist', index);
     },
-    play: function (index) {
+
+    play (index) {
       this.$dispatch('playPlaylist', index);
     },
-    delete: function (e) {
+
+    delete (e) {
       if (e.keyCode !== 8 && e.keyCode !== 46) { return; }
       this.$dispatch('removePlaylists', this.selectedPlaylists);
       this.selectedPlaylists = [];
     },
-    deselectOthers: function (index) {
+
+    deselectOthers (index) {
       this.selectedPlaylists = [index + ""];
     },
-    click: function (e, index, isDraggableElement) {
-      var now = new Date().getTime();
+
+    click (e, index, isDraggableElement) {
+      const now = new Date().getTime();
       if (this.lastClick.index === index) {
         if (now - this.lastClick.time < 500) {
           this.play(index);
@@ -80,69 +91,81 @@ var Sidebar = Vue.extend({
           this.editName(index);
         }
       }
-      this.lastClick.time = now;
+      this.lastClick.time  = now;
       this.lastClick.index = index;
 
       if (isDraggableElement) {
         this.deselectOthers(index);
       }
     },
-    editName: function (index) {
+
+    editName (index) {
       this.$broadcast('editName', index);
     },
-    onDragStart:function (e) {
+
+    onDragStart (e) {
       e.dataTransfer.setData('draggingPlaylistsNumber', this.selectedPlaylists.length);
       this.dragOpts.optionHeight = this.$el.querySelector('option.option').getBoundingClientRect().height;
       this.isDragging = true;
       this.dragOpts.e = e;
-      this.dragOpts.timer = setInterval(this.movePlaylists.bind(this), 20);
+      this.dragOpts.timer = setInterval(::this.movePlaylists, 20);
     },
-    onDragEnd: function (e) {
-      this.isDragging = false;
+
+    onDragEnd () {
+      this.isDragging         = false;
       this.dragOpts.insertPos = null;
+
       clearInterval(this.dragOpts.timer);
-      this.dragOpts.elementsAfterInsert.forEach(function (e) {
+
+      this.dragOpts.elementsAfterInsert.forEach((e) => {
         e.style.marginTop = '0px';
       });
     },
-    onDragOver: function (e) {
-      var items = e.dataTransfer.getData('draggingPlaylistsNumber');
-      this.dragOpts.e = e;
-      this.dragOpts.items = items;
+
+    onDragOver (e) {
+      this.dragOpts.e     = e;
+      this.dragOpts.items = e.dataTransfer.getData('draggingPlaylistsNumber');
 
       // this is needed for onDrop fires
       e.preventDefault();
       e.stopPropagation();
     },
-    onDrop: function (e) {
-      var rect = this.$$.real.getBoundingClientRect();
-      var newPos = ((e.clientY - rect.top) / this.dragOpts.optionHeight) | 0;
+
+    onDrop (e) {
+      const rect   = this.$$.real.getBoundingClientRect();
+      const newPos = ((e.clientY - rect.top) / this.dragOpts.optionHeight) | 0;
       this.$dispatch('movePlaylists', this.selectedPlaylists, newPos);
       this.selectedPlaylists = [];
     },
-    movePlaylists: function () {
+
+    movePlaylists () {
       if (!this.dragOpts.e || !this.dragOpts.items) { return; }
-      var opts = this.dragOpts;
-      var rect = this.$$.real.getBoundingClientRect();
-      var newPos = ((opts.e.clientY - rect.top) / opts.optionHeight) | 0;
+
+      const opts   = this.dragOpts;
+      const rect   = this.$$.real.getBoundingClientRect();
+      const newPos = ((opts.e.clientY - rect.top) / opts.optionHeight) | 0;
+
       if (opts.insertPos === newPos) { return; }
+
       opts.insertPos = newPos;
-      opts.elementsAfterInsert.forEach(function (e) {
+      opts.elementsAfterInsert.forEach((e) => {
         e.style.marginTop = '0px';
       });
 
       if (newPos < this.collection.size()) {
-        var newElements = [];
-        var playlistElements = this.$el.querySelectorAll('.SidebarPlaylist');
-        [].forEach.call(playlistElements, function (e) {
+        const newElements = [];
+        const playlistElements = this.$el.querySelectorAll('.SidebarPlaylist');
+        [].forEach.call(playlistElements, (e) => {
           var newE = e.querySelectorAll('.option')[opts.insertPos];
           newE.style.marginTop = (opts.optionHeight * opts.items) + 'px';
           newElements.push(newE);
         });
         opts.elementsAfterInsert = newElements;
       }
-    }
-  }
+    },
+
+  },
+
 });
 
-module.exports = Sidebar;
+export default Sidebar;
