6cedb6204747273d3603a7b61fa5fc11217ad0c0
fand <fand@gmork.in>
2015-10-21 22:27:41 +0900

    Rewrite Song.js in ES6

--- src/scripts/models/Song.js
+++ src/scripts/models/Song.js
@@ -1,11 +1,11 @@
-'use strict';
+/* global PouchDB */
 
-var fs = require('fs');
-var mime = require('mime');
-var songDB = PouchDB('song');
+import fs   from 'fs';
+import mime from 'mime';
+const songDB = PouchDB('song');
 
-var checker = document.createElement('audio');
-var isLoadable = function (path) {
+const checker = document.createElement('audio');
+const isLoadable = (path) => {
   if (fs.existsSync(path)) {
     var type = mime.lookup(path);
     return !!(checker.canPlayType(type).replace('no', ''));
@@ -13,62 +13,63 @@ var isLoadable = function (path) {
   return false;
 };
 
-var Song = function (opts) {
-  this.name = opts.name;
-  this.path = opts.path;
-  this.rate = opts.rate;
-  this.duration = opts.duration;
-  this._id = this.path;
-  if (opts._rev) { this._rev = opts._rev; }
-};
-Song.prototype.save = function () {
-  var opts = {
-    _id: this._id,
-    name: this.name,
-    path: this.path,
-    rate: this.rate,
-    duration: this.duration
-  };
+class Song {
 
-  return songDB.get(opts._id)
-    .then(function (doc) {
-      return opts._rev = doc._rev;
-    })
-    .catch(function (err) {
-      if (err.status !== 404) { throw err; }
-      return opts._rev = self._rev;
-    })
-    .then(function (doc) {
-      return songDB.put(opts);
-    })
-    .catch(function (err) {
-      if (err.status !== 404) {
-        console.error('song save error');
-        console.error(err);
-      }
-    })
-    .then(function () {
-      return self;
-    });
-};
+  constructor (opts) {
+    this.name     = opts.name;
+    this.path     = opts.path;
+    this.rate     = opts.rate;
+    this.duration = opts.duration;
+    this._id      = this.path;
+
+    if (opts._rev) {
+      this._rev = opts._rev;
+    }
+  }
+
+  save () {
+    var opts = {
+      _id      : this._id,
+      name     : this.name,
+      path     : this.path,
+      rate     : this.rate,
+      duration : this.duration,
+    };
+
+    return songDB.get(opts._id)
+      .then((doc) => {
+        opts._rev = doc._rev;
+      })
+      .catch((err) => {
+        if (err.status !== 404) { throw err; }
+        opts._rev = self._rev;
+      })
+      .then(() => songDB.put(opts))
+      .catch((err) => {
+        if (err.status !== 404) {
+          console.error('song save error');
+          console.error(err);
+        }
+      })
+      .then(() => this);
+  }
+
+}
 
 // Factory
 Song.create = function (file) {
-  var song;
-  if (isLoadable(file.path)) {
-    song = new Song({
-      name: file.name,
-      path: file.path,
-      rate: 1.0,
-      duration: null
-    });
-  }
-  return song;
+  if (!isLoadable(file.path)) { return; }
+  return new Song({
+    name     : file.name,
+    path     : file.path,
+    rate     : 1.0,
+    duration : null,
+  });
 };
-Song.load = function (id) {
+
+Song.load = (id) => {
   return songDB.get(id)
-    .then(function (opts) {
-      return new Song(opts);
-    });
+    .then((opts) => new Song(opts));
 };
-module.exports = Song;
+
+export default Song;
